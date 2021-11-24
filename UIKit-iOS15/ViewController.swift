c3ecd88333b63e469acccb602bd790b9e7d33b4b
KAMONPAN KAEWHEANG <kamonpan@dit.daikin.co.jp>
2021-11-24 19:14:50 +0700

    Initial Commit

---
+++ UIKit-iOS15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-iOS15
+//
+//  Created by admin on 24/11/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
